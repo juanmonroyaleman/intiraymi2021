f84f0e51599798b5865f42b06a2b9b73ce369eda
Juan <juancarlos22.09.96@gmail.com>
2020-07-25 04:05:09 -0500

    “intiraymi2021”

---
+++ js/app.js
@@ -0,0 +1,8 @@
+
+var burger = document.querySelector(".header__burger");
+var menu = document.querySelector(".menu");
+
+burger.addEventListener("click", function(){
+		menu.classList.toggle("menu--visible")
+})
+
